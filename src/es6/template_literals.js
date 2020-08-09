92ee13e6da991a685801399051a7869353e35a4c
Miguel Prada <mapra99@gmail.com>
2020-08-08 23:58:57 -0500

    template literals

---
+++ src/es6/template_literals.js
@@ -0,0 +1,10 @@
+let hello = 'Hello';
+let world = 'World';
+
+// before es6
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// es6
+let epicPhrase = `${hello} ${world}`
+console.log(epicPhrase);
